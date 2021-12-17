6cf46a574b575a847aeca2fcb7c2f8fcf894c59a
Huno Jung <78013523+hunojung@users.noreply.github.com>
2021-12-17 18:29:23 +0900

    ADD Algorithm solving

---
+++ BaekJoon/CLASS 1/11720.java
@@ -0,0 +1,26 @@
+// 11720
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int number = Integer.parseInt(br.readLine());
+		String input = br.readLine();
+		int sum = 0;
+    
+		for(int i = 0; i < number; i++){
+			sum+=input.charAt(i)-'0';
+		}
+		
+		System.out.println(sum);
+		
+		br.close();
+		System.exit(0);
+
+	}
+}
+// 11720 end
