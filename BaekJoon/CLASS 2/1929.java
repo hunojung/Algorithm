be58ebceffd5cc80c71f9500ef37787b595b3e11
Huno Jung <78013523+hunojung@users.noreply.github.com>
2021-12-20 11:48:03 +0900

    21-12-20 Algorithm solving

---
+++ BaekJoon/CLASS 2/1929.java
@@ -0,0 +1,36 @@
+// 소수 찾기
+// 1929
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int M = Integer.parseInt(st.nextToken());
+		int N = Integer.parseInt(st.nextToken());
+		boolean sosu = true;
+		
+		while(M <= N){
+			
+			sosu = true;
+			if(M > 2)
+				for (int i = 2; i < Math.sqrt(M)+1; i++) {
+					if(i>2 && i%2==0)i++;
+					if(M%i == 0){
+						sosu = false;
+						break;
+					}
+				}
+			
+			if(sosu && M!=1)System.out.println(M);
+			M++;
+		}
+		br.close();
+		System.exit(0);
+	}
+}
+// 1929 END
