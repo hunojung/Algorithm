d40ebfa3a407eda8ab94277c6dc2d9403422c473
Huno Jung <78013523+hunojung@users.noreply.github.com>
2021-12-29 12:01:13 +0900

    ADD min,max remove

--- BaekJoon/CLASS 3/1107_Brute_Force.java
+++ BaekJoon/CLASS 3/1107_Brute_Force.java
@@ -1,4 +1,68 @@
 // 리모콘
+
+// 1107 min max 할 필요 없네..
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.HashSet;
+import java.util.StringTokenizer;
+
+public class Main {
+	
+	public static void main(String[] args) throws Exception {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		// 값 입력
+		int num = Integer.parseInt(br.readLine()); // 목표 채널
+		int testcase = Integer.parseInt(br.readLine()); // 부서진 버튼 수
+		HashSet<Integer> broken = new HashSet<>(); // 부서진 버튼 저장할 변수
+		StringTokenizer st = null;
+		if(testcase != 0) st = new StringTokenizer(br.readLine());
+		
+		for (int i = 0; i < testcase; i++) {
+			broken.add(Integer.parseInt(st.nextToken()));		
+		}
+		// 값 입력 끝
+		
+		// min, max 및 다른 설정
+		int count = Math.abs(num-100);
+		
+		for (int i = 0 ; i < 1000001; i++) {
+			
+			// broken이 숫자에 포함되었나
+			int numI=i;
+
+			int len=0; // 버튼 누른 횟수 넣기
+			int lenI=i;
+			while(lenI>0){len++;lenI/=10;}
+			if(i==0)len=1;
+			boolean contain = false;
+			for (int j = 0; j < len; j++) {
+				if(broken.contains(numI%10)){
+					contain=true;
+					continue;
+				}else{
+					numI/=10;
+				}
+			}
+			if(contain)continue;
+			
+			int countNext = Math.abs(num-i)+len; // count 비교할 변수
+			if(count>countNext){ // count 비교
+				count=countNext;
+				//System.out.println(num+" "+i+" "+lenI);
+			}
+		}
+		
+		System.out.println(count);
+		
+		br.close();
+		System.exit(0);
+	}
+}
+
+
+
 // 1107
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
