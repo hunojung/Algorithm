0efe9098027ede50b37033afe33417907b32b592
Huno Jung <78013523+hunojung@users.noreply.github.com>
2022-01-04 17:45:14 +0900

    ADD exception

--- BaekJoon/CLASS 3/2579_DP.java
+++ BaekJoon/CLASS 3/2579_DP.java
@@ -15,13 +15,20 @@ public class Main {
 			stair[i]=Integer.parseInt(br.readLine());
 		}
 		
+		
 		// 각각의 index까지 최댓값을 계속 누적해온다?
 		// 그렇게되면 특정 index에서 최댓값은 
 		// 해당 인덱스 + ( sum(index-3) + index-1 합 or sum(index-2) ) 
 		int[] sum = new int[N];
 		sum[0] = stair[0];
-		sum[1] = stair[1]+sum[0];
-		sum[2] = stair[2]+Math.max(stair[0], stair[1]);
+		
+		if(N >= 2){
+			sum[1] = stair[1]+sum[0];
+		}
+		
+		if (N > 2) {
+			sum[2] = stair[2]+Math.max(stair[0], stair[1]);
+		}
 		
 		// 10 , 20 , 15 , 25 , 10 , 20
 		for (int i = 3; i < N; i++) {
