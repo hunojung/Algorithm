dc5ee35f3700f037805c88de086961ac42d98a8c
Huno Jung <78013523+hunojung@users.noreply.github.com>
2022-01-05 15:42:31 +0900

    22-01-05 Algorithm solving

--- BaekJoon/CLASS 3/5430_Deque.java
+++ BaekJoon/CLASS 3/5430_Deque.java
@@ -7,10 +7,6 @@ import java.util.ArrayList;
 import java.util.StringTokenizer;
 
 public class Main {
-	public static int countChar(String str, char ch) {
-		return str.length() - str.replace(String.valueOf(ch), "").length();
-	}
-  
 	public static void main(String[] args) throws Exception {
 		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
