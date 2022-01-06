2ecb20b9cf91084075145ab667617e5c16abdc2e
Huno Jung <78013523+hunojung@users.noreply.github.com>
2022-01-06 12:19:05 +0900

    22-01-06 Algorithm solving

--- BaekJoon/CLASS 3/7569_7576_BFS.java
+++ BaekJoon/CLASS 3/7569_7576_BFS.java
@@ -1,5 +1,5 @@
-// 토마토
-// 7569
+// 토마토 
+// 7569 3
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.LinkedList;
@@ -126,3 +126,100 @@ public class Main {
 	}
 }
 // 7569 END
+
+//7576 2차원
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class Main {
+	static int[][] tomato;
+	static int M;
+	static int N;
+	static int H;
+	
+	public static void main(String[] args) throws Exception {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		// 값 입력
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		M = Integer.parseInt(st.nextToken());
+		N = Integer.parseInt(st.nextToken());
+
+		Queue<Integer> q = new LinkedList<>();
+
+		tomato = new int[M][N];
+
+		for (int j = 0; j < N; j++) {
+			st = new StringTokenizer(br.readLine());
+			for (int j2 = 0; j2 < M; j2++) {
+				tomato[j2][j]=Integer.parseInt(st.nextToken());
+			}
+		}
+	
+		br.close();
+
+		for (int j = 0; j < N; j++) {
+			for (int j2 = 0; j2 < M; j2++) {
+				if(tomato[j2][j]==1){
+					//System.out.println(j+" "+j2+" "+i);
+					q.add(j2);
+					q.add(j);
+				}
+			}
+		}
+		
+		bfs(q);
+		
+		int maxC = 0; // max count 값
+		for (int j = 0; j < N; j++) {
+			for (int j2 = 0; j2 < M; j2++) {
+				if(maxC < tomato[j2][j]) maxC = tomato[j2][j];
+				
+				if(tomato[j2][j]==0){
+					System.out.println(-1);
+					System.exit(0);
+					
+				}
+			}
+		}
+		
+		System.out.println(maxC-1);
+		
+		br.close();
+		System.exit(0);
+	}
+	
+	public static void bfs(Queue<Integer> q){
+		
+		while(!q.isEmpty()){
+			int x = q.poll();
+			int y = q.poll();
+			
+			if(x>0 && tomato[x-1][y] == 0 ){
+				q.add(x-1);
+				q.add(y);
+				tomato[x-1][y] = tomato[x][y]+1;
+			}
+			
+			if(x+1<M && tomato[x+1][y] == 0 ){
+				q.add(x+1);
+				q.add(y);
+				tomato[x+1][y] = tomato[x][y]+1;
+			}
+			if(y>0 && tomato[x][y-1] == 0){
+				q.add(x);
+				q.add(y-1);
+				tomato[x][y-1] = tomato[x][y]+1;
+			}
+			if(y+1<N && tomato[x][y+1] == 0){
+				q.add(x);
+				q.add(y+1);
+				tomato[x][y+1] = tomato[x][y]+1;
+			}
+		}
+	}
+}
+// 7576 END
