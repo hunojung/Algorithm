13b2159edf37bf15ecd95aa6e80f4dd068dfd346
Huno Jung <78013523+hunojung@users.noreply.github.com>
2022-01-14 01:06:58 +0900

    Programmers Level 2

--- playdata/study 1/bracket_conversion.java
+++ playdata/study 1/bracket_conversion.java
@@ -1,57 +1,59 @@
 // 괄호 변환
 class Solution {
     public String solution(String p) {
-    	
-    	//System.out.println(p.substring(1,p.length()-1));
         String answer = "";
         String answer_last="";
         String u = "";
         String v = "";
         
-        while(true){
-        	boolean alright = false;
-        	int N = p.length();
-        	int count = 0;
-        	for (int i = 0; i < N; i++) {
-				if(p.charAt(i)=='('){
-					count++;
-					// 분리된 u 가 정상인지 
-					if(count>0)alright=true;
-				}
-				else{
-					count--;
-				}
-				if(count==0){
-					u = p.substring(0, i+1);
-					v = p.substring(i+1,N);
-					break;
-				}
+        // 공백이 들어오면 리턴
+        if(p.equals(""))return "";
+        
+        // u가 정상인지 판단
+        boolean alright = false;
+        
+    	int N = p.length();
+    	int count = 0;
+    	
+    	// u와 v로 나눈다 . 나누면서 count가 음수이면 ')'가 많았다는 것
+    	for (int i = 0; i < N; i++) {
+			if(p.charAt(i)=='('){
+				count++;
+				// 분리된 u 가 정상인지 
+				if(count>0)alright=true;
 			}
-        	
-        	// u 가 정상인가
-        	if(alright){
-        		answer = answer+u;
-        		if(v.length()==0)break;
-        	// u 가 비정상인가
-        	}else{
-        		int len = u.length();
-        		String u_copy ="";
-        		if(len>2){
-        			u = u.substring(1,len-1);
-        			for (int i = 0; i < len-2; i++) {
-        				if(u.charAt(i)=='('){
-        					u_copy = u_copy+")";
-        				}else{
-        					u_copy = u_copy+"(";
-        				}
-        			}
-        		}
-        		answer = answer+"(";
-        		answer_last = ")"+u_copy+answer_last;
-        		if(v.length()==0)break;
-        	}
-        	p=v;
-        }
-        return answer+answer_last;
+			else{
+				count--;
+			}
+			if(count==0){
+				u = p.substring(0, i+1);
+				v = p.substring(i+1,N);
+				break;
+			}
+		}
+    	
+    	// u 가 정상
+    	if(alright){
+    		answer = answer+u;
+    		
+    	// u 가 비정상
+    	}else{
+    		int len = u.length();
+    		String u_copy ="";
+    		if(len>2){
+    			u = u.substring(1,len-1);
+    			for (int i = 0; i < len-2; i++) {
+    				if(u.charAt(i)=='('){
+    					u_copy = u_copy+")";
+    				}else{
+    					u_copy = u_copy+"(";
+    				}
+    			}
+    		}
+    		answer = answer+"(";
+    		answer_last = ")"+u_copy+answer_last;
+    	}
+        
+        return answer+solution(v)+answer_last;
     }
 }
