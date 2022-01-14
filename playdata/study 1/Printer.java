04971efcfce7b42e90fbf28923bf224b5d84a010
Huno Jung <78013523+hunojung@users.noreply.github.com>
2022-01-14 11:15:08 +0900

    Programmers Level 2

--- playdata/study 1/Printer.java
+++ playdata/study 1/Printer.java
@@ -14,39 +14,38 @@ public class Solution{
 
 		
 	}
+	
     public int solution(int[] priorities, int location) {
     	int answer = 1;
     	Queue<Print> q = new LinkedList<>();
-    	PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
         for (int i = 0; i < priorities.length; i++) {
         	q.add(new Print(i,priorities[i]));
-        	pq.add(priorities[i]);
 		}
         
-        Print next = null;
-        int comp;
+        Arrays.sort(priorities);
         
+        Print next = null;
+        int size = priorities.length;
         while(true){
 			
         	next = q.poll();
-			comp = pq.poll();
 			//System.out.println(next.priorities+" "+next.location);
 			
 			// priorities가 최댓값이고 로케이션이 맞으면 출력
-			if(comp == next.priorities){
+			if(priorities[size-answer] == next.priorities){
 				if(next.location==location){
 					break;
 				
-				// priorities가 안맞으면 다시 추가
 				}else{
 					answer++;
 				}
-			// priorities 최대 아니면 다시
+				
+			// priorities 최대 아니면 다시넣어
 			}else{
 				q.add(next);
-				pq.add(comp);			
 			}
 		}
+        
         return answer;
     }
 }
