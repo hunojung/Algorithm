3d0b9b67d3092cf8bf5d35e6ae557be647ba0c1c
Huno Jung <78013523+hunojung@users.noreply.github.com>
2022-02-01 15:31:46 +0900

    22-01-23 https://programmers.co.kr/learn/courses/30/lessons/92335

--- Programmers/2022_KAKAO_BLIND_RECRUITMENT/k진수에서_소수_개수_구하기.java
+++ Programmers/2022_KAKAO_BLIND_RECRUITMENT/k진수에서_소수_개수_구하기.java
@@ -1,8 +1,7 @@
-// https://programmers.co.kr/learn/courses/30/lessons/92335#qna
+// https://programmers.co.kr/learn/courses/30/lessons/92335
 class Solution {
     public int solution(int n, int k) {
         int answer = 0;
-        
         String[] num = Integer.toString(n,k).split("0+");
         for(String s : num){
             // 다음 숫자
