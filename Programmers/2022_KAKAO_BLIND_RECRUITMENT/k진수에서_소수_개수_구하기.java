5f77e4572fb5b18535a649e6f8dcf968dca3f051
Huno Jung <78013523+hunojung@users.noreply.github.com>
2022-02-01 15:31:22 +0900

    22-01-23 https://programmers.co.kr/learn/courses/30/lessons/92335#qna

---
+++ Programmers/2022_KAKAO_BLIND_RECRUITMENT/k진수에서_소수_개수_구하기.java
@@ -0,0 +1,27 @@
+// https://programmers.co.kr/learn/courses/30/lessons/92335#qna
+class Solution {
+    public int solution(int n, int k) {
+        int answer = 0;
+        
+        String[] num = Integer.toString(n,k).split("0+");
+        for(String s : num){
+            // 다음 숫자
+            if(isPrime(s))answer++;
+        }
+        return answer;
+    }
+    
+    public boolean isPrime(String n){
+        Long num = Long.parseLong(n);
+        
+        if(num==1 || num==4)
+            return false;
+        else if(num==2 || num==3)
+            return true;
+        
+        for(long i = 2 ; i*i<=num ; i++ )
+            if(num%i==0)
+                return false;
+        return true;
+    }
+}
