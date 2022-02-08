ea22199d3caf194a94ea403007e106c1061f3e7c
HunoJung <78013523+hunojung@users.noreply.github.com>
2022-02-08 16:54:04 +0900

    22-02-08 Easy https://leetcode.com/problems/search-insert-position/

---
+++ Leetcode/0035-search-insert-position.java
@@ -0,0 +1,18 @@
+// https://leetcode.com/problems/search-insert-position/
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        int left = 0, right = nums.length-1,mid;
+        while(left<=right){
+            mid=(left+right)/2;
+            
+            if(nums[mid]<target){
+                left = mid+1;
+            }else if(nums[mid]>target){
+                right = mid-1;
+            }else{
+                return mid;
+            }
+        }
+        return left;
+    }
+}
