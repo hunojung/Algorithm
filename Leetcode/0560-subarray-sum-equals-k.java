fa7783045f9e65072e08299b4387d12c69e1570d
HunoJung <78013523+hunojung@users.noreply.github.com>
2022-02-11 00:41:52 +0900

    22-02-10 Medium https://leetcode.com/problems/subarray-sum-equals-k/

---
+++ Leetcode/0560-subarray-sum-equals-k.java
@@ -0,0 +1,15 @@
+// https://leetcode.com/problems/subarray-sum-equals-k/
+class Solution {
+    public int subarraySum(int[] nums, int k) {
+    	
+    	int res = 0, sum=0;
+    	HashMap<Integer,Integer> map = new HashMap<>();
+    	map.put(0,1);
+        for(int i = 0; i < nums.length ; i++){
+    		sum += nums[i];
+    		res += map.getOrDefault((sum-k),0);
+    		map.put(sum, map.getOrDefault(sum, 0)+1);
+    	}
+    	return res;
+    }
+}
