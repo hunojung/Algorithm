af99eda337f1418272a162de2d6ab674d6542c7d
HunoJung <78013523+hunojung@users.noreply.github.com>
2022-02-19 23:23:06 +0900

    22-02-19 https://www.acmicpc.net/problem/15666

---
+++ BaekJoon/Class_4/15666_BackTacking.java
@@ -0,0 +1,44 @@
+// https://www.acmicpc.net/problem/15666
+// N과 M (12)
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	static StringBuilder sb;
+	public static void main (String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		// 입력
+		sb = new StringBuilder();
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+		int[] nums = new int[N];
+		st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < N; i++) {
+			nums[i]=Integer.parseInt(st.nextToken());
+		}
+		Arrays.sort(nums);
+		BackTracking(nums,new int[M],0,0,M);
+		
+		System.out.println(sb);
+		br.close();
+		System.exit(0);
+	}
+	
+	static void BackTracking(int[] nums,int[] res,int start,int resIndex,int M){
+		if(resIndex==M){
+			for (int i = 0; i < resIndex; i++) {
+				sb.append(res[i]+" ");
+			}
+			sb.append("\n");
+			return;
+		}
+		int before = 0;
+		for (int i = start; i < nums.length; i++) {
+			if(before==nums[i])continue;
+			res[resIndex]=nums[i];
+			BackTracking(nums,res,i,resIndex+1,M);
+			before=nums[i];
+		}
+	}
+}
