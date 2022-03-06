28e8526edc1f23607efaffb75c4ea1cf525a05ac
HunoJung <78013523+hunojung@users.noreply.github.com>
2022-03-07 00:53:32 +0900

    22-03-06 Hard https://leetcode.com/problems/count-all-valid-pickup-and-delivery-options/

---
+++ Leetcode/1359-count-all-valid-pickup-and-delivery-options.java
@@ -0,0 +1,10 @@
+// https://leetcode.com/problems/count-all-valid-pickup-and-delivery-options/
+class Solution {
+    public int countOrders(int n) {
+        long ans = 1;
+        for(int i=2;i<n+1;i++){
+            ans=(ans*(i*2)*(i*2-1)/2)%1000000007;
+        }
+        return (int)ans;
+    }
+}
