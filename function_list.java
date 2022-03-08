903f6d7b5636897c4e8e06f0ed015be0de3e29c2
HunoJung <78013523+hunojung@users.noreply.github.com>
2022-03-09 00:20:43 +0900

    Update function_list.java

--- function_list.java
+++ function_list.java
@@ -1,3 +1,46 @@
+// Union_Find
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	static int ans = 0;
+	public static void main (String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int n = Integer.parseInt(st.nextToken());
+		int m = Integer.parseInt(st.nextToken());
+		int ans = 0;
+		int[] g = new int[n];
+		
+		for (int i = 0; i < n; i++) {
+			g[i]=i;
+		}
+		
+		for (int i = 0; i < m; i++) {
+			st = new StringTokenizer(br.readLine());
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			
+			a = find(g,a);
+			b = find(g,b);
+			if(a == b){
+				ans = i+1;
+				break;
+			}else{
+				g[b] = a;
+			}
+		}
+		System.out.println(ans);
+		br.close();
+		System.exit(0);
+	}
+	
+	static int find(int[] g,int n){
+		if(g[n]==n) return n;
+		return g[n] = find(g,g[n]);
+	}
+}
+-------------------------------------------------------------------------
 // 다익스트라
 	static int dijkstra (int path,int goal){
 		
