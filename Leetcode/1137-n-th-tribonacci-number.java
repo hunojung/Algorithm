9cf526f47dd228b2a0cc2ba5ec92c74ae91563f3
HunoJung <78013523+hunojung@users.noreply.github.com>
2022-05-11 08:55:11 +0900

    22-05-11 Easy https://leetcode.com/problems/n-th-tribonacci-number/

---
+++ Leetcode/1137-n-th-tribonacci-number.java
@@ -0,0 +1,15 @@
+// https://leetcode.com/problems/n-th-tribonacci-number/
+class Solution {
+    public int tribonacci(int n) {
+        if(n<2) return n;
+        
+        int[] t = new int[n+1];
+        t[1]=1;
+        t[2]=1;
+        
+        for(int i=3;i<n+1;i++)
+            t[i]=t[i-1]+t[i-2]+t[i-3];
+        
+        return t[n];
+    }
+}
