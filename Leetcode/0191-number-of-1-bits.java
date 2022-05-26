35ec31f8a3e85ad51205e311b2f2a4d70322b555
HunoJung <78013523+hunojung@users.noreply.github.com>
2022-05-26 21:51:11 +0900

    22-05-26 Easy https://leetcode.com/problems/number-of-1-bits/

---
+++ Leetcode/0191-number-of-1-bits.java
@@ -0,0 +1,21 @@
+// https://leetcode.com/problems/number-of-1-bits/
+public class Solution {
+    // you need to treat n as an unsigned value
+    public int hammingWeight(int n) {
+        
+        int count = 0;
+        
+        //System.out.println("n : "+n);
+        if(n<0){
+            count++;
+            n = Integer.MAX_VALUE+(n+1);
+        }
+        
+        while(n>0){
+            //System.out.println(n+" "+count);
+            if(n%2==1) count++;
+            n/=2;
+        }
+        return count;
+    }
+}
