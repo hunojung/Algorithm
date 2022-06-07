a88f88fbf73c5f0a89597218b6457916f198c1e0
HunoJung <78013523+hunojung@users.noreply.github.com>
2022-06-08 00:19:40 +0900

    22-06-08 Easy Update

--- Leetcode/0191-number-of-1-bits.java
+++ Leetcode/0191-number-of-1-bits.java
@@ -2,6 +2,21 @@
 public class Solution {
     // you need to treat n as an unsigned value
     public int hammingWeight(int n) {
+        int one = 0;
+        
+        while(n!=0){
+            one += (n & 1);            
+            n>>>=1; // unsigned int shift
+        }
+        
+        return one;
+    }
+}
+
+
+public class Solution {
+    // you need to treat n as an unsigned value
+    public int hammingWeight(int n) {
         
         int count = 0;
         
