7007a3e1f0f3369d0309af9d0316a563c32b36fb
HunoJung <78013523+hunojung@users.noreply.github.com>
2022-06-10 22:39:56 +0900

    220-06-10 Medium Update

--- Leetcode/0003-longest-substring-without-repeating-characters.java
+++ Leetcode/0003-longest-substring-without-repeating-characters.java
@@ -1,4 +1,26 @@
 // https://leetcode.com/problems/longest-substring-without-repeating-characters/
+// update
+class Solution {
+    public int lengthOfLongestSubstring(String s) {
+        int answer = 0;
+        int left = 0;
+        Map<Character,Integer> map = new HashMap<>();
+        
+        for(int i=0;i<s.length();i++){
+            char now = s.charAt(i);
+            
+            if(map.containsKey(now)){
+                left = Math.max(left,map.get(now)+1);
+            }
+            
+            map.put(now,i);
+            answer = Math.max(answer,i-left+1);
+        }
+        
+        return answer;
+    }
+}
+
 class Solution {
     public int lengthOfLongestSubstring(String s) {
         int answer = 0;
