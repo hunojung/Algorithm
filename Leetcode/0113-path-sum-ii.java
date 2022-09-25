f3da397e19306b5156ee62c656692e3d323a2533
HunoJung <78013523+hunojung@users.noreply.github.com>
2022-09-25 23:56:13 +0900

    22-09-25 Medium https://leetcode.com/problems/path-sum-ii/

--- Leetcode/0113-path-sum-ii.java
+++ Leetcode/0113-path-sum-ii.java
@@ -1,4 +1,33 @@
 // https://leetcode.com/problems/path-sum-ii/
+// 이게 메모리가 낮다
+class Solution {
+    public List<List<Integer>> pathSum(TreeNode root, int targetSum) {
+        List<List<Integer>> answer = new ArrayList<>();
+        
+        checkPath(answer,root,targetSum,new ArrayList<>());
+            
+        return answer;
+    }
+    
+    private void checkPath(List<List<Integer>> answer, TreeNode root, int sum, List<Integer> list){
+        if(root == null) return;
+        
+        list.add(root.val);
+        
+        if(root.left == null && root.right == null && sum-root.val == 0){
+            if(root.left == null && root.right == null){
+                answer.add(new ArrayList<>(list));
+            }
+        }
+        
+        checkPath(answer,root.left, sum-root.val, list);
+        checkPath(answer,root.right, sum-root.val, list);
+        
+        list.remove(list.size()-1);
+    }
+}
+
+// first submit
 class Solution {
     private List<List<Integer>> answer;
     private int targetSum;
